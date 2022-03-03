16d36d1b6e62e29ea1ff36feae09b4baf7a98831
tatarmagda <tatar.magdalena@wp.pl>
2022-03-03 15:32:07 +0100

    cleaning

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
@@ -13,9 +9,6 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: Center(
         child: Column(),
       ),
---
+++ lib/permission_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PermissionScreen extends StatefulWidget {
+  @override
+  State<PermissionScreen> createState() => _PermissionScreenState();
+}
+
+class _PermissionScreenState extends State<PermissionScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(),
+      ),
+    );
+  }
+}
