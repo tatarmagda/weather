6d7b4ba592d7c2ce2fd36871db4b3ce723786dbf
tatarmagda <tatar.magdalena@wp.pl>
2022-03-03 15:50:23 +0100

    image

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -13,11 +13,36 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Container(),
+          Align(
+            alignment: FractionalOffset.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.air,
+                  color: Color.fromARGB(255, 84, 104, 221),
+                  size: 70,
+                ),
+                Padding(padding: EdgeInsets.only(top: 15.0)),
+                Text(widget.title),
+                Padding(padding: EdgeInsets.only(top: 5.0)),
+                Text("Aplikacja do monitorowania \n powietrza"),
+              ],
+            ),
+          ),
+          Positioned(
+              left: 0,
+              right: 0,
+              bottom: 50,
+              child: Container(
+                alignment: Alignment.center,
+                child: Text("Ladowanie danych...."),
+              )),
+        ],
       ),
     );
   }
