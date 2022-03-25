1d46004048e3cdb9c36c31b29832b19927f9c165
tatarmagda <tatar.magdalena@wp.pl>
2022-03-25 13:01:47 +0100

    adding elements to air screen

--- lib/air_screen.dart
+++ lib/air_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:pogoda/main.dart';
@@ -16,7 +18,7 @@ class _AirScreenState extends State<AirScreen> {
         children: [
           Container(
             decoration: BoxDecoration(
-              color: new Color(0xffffffff),
+              color: const Color(0xffffffff),
               gradient: LinearGradient(
                   begin: Alignment.centerRight,
                   end: Alignment.centerLeft,
@@ -31,45 +33,199 @@ class _AirScreenState extends State<AirScreen> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Image(
-                  image: AssetImage("icons/cloud-sun.png"),
+                Text("Jakość powietrza"),
+                Padding(
+                  padding: EdgeInsets.only(top: 2),
                 ),
-                Padding(padding: EdgeInsets.only(top: 10.0)),
-                Text(
-                  Strings.appTitle,
-                  textAlign: TextAlign.center,
-                  style: GoogleFonts.lato(
-                    textStyle: TextStyle(fontSize: 30.0),
-                    color: Color.fromARGB(255, 24, 56, 105),
-                    fontWeight: FontWeight.bold,
+                Text("Bardzo dobra"),
+                Padding(
+                  padding: EdgeInsets.only(top: 24),
+                ),
+                CircleAvatar(
+                  radius: 92,
+                  child: Center(
+                    child: Container(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "45",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 44),
+                          ),
+                          Text(
+                            "CAQI ⓘ",
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                 ),
-                Padding(padding: EdgeInsets.only(top: 5.0)),
+                Padding(
+                  padding: EdgeInsets.only(top: 30),
+                ),
+                IntrinsicHeight(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          width: 130,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Text(
+                                "PM 2,5",
+                                textAlign: TextAlign.center,
+                                style: GoogleFonts.lato(
+                                    textStyle: TextStyle(fontSize: 16.0),
+                                    height: 1.2,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(top: 2.0),
+                              ),
+                              Text(
+                                "10%",
+                                textAlign: TextAlign.center,
+                                style: GoogleFonts.lato(
+                                    textStyle: TextStyle(fontSize: 20.0),
+                                    height: 1.2,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      VerticalDivider(
+                        width: 48.0,
+                        thickness: 1.0,
+                        color: Colors.white,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          width: 130,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(
+                                "PM 10",
+                                textAlign: TextAlign.center,
+                                style: GoogleFonts.lato(
+                                    textStyle: TextStyle(fontSize: 16.0),
+                                    height: 1.2,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(top: 2.0),
+                              ),
+                              Text(
+                                "12%",
+                                textAlign: TextAlign.center,
+                                style: GoogleFonts.lato(
+                                    textStyle: TextStyle(fontSize: 20.0),
+                                    height: 1.2,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
                 Text(
-                  "jakosc powietrza",
+                  "Stacja pomiarowa",
                   textAlign: TextAlign.center,
-                  style: GoogleFonts.lato(
-                      textStyle: TextStyle(
-                          fontSize: 20.0, fontWeight: FontWeight.bold),
-                      color: Colors.white),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 2.0),
+                ),
+                Text(
+                  "Warszawa",
+                  textAlign: TextAlign.center,
                 ),
               ],
             ),
           ),
           Positioned(
-              left: 0,
-              right: 0,
-              bottom: 50,
-              child: Container(
-                alignment: Alignment.center,
-                child: Text(
-                  "Ladowanie danych....",
-                  style: GoogleFonts.lato(
-                    textStyle: TextStyle(fontSize: 15.0),
-                    color: Color.fromARGB(255, 24, 56, 105),
+            left: 8,
+            right: 0,
+            bottom: 0,
+            top: 0,
+            child: Container(
+              alignment: Alignment.centerLeft,
+              child: Stack(
+                children: [
+                  ClipRect(
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      heightFactor: 1,
+                      child: Image.asset(
+                        "icons/danger-value-negative.png",
+                        scale: 0.9,
+                      ),
+                    ),
                   ),
-                ),
-              )),
+                  ClipRect(
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      heightFactor: 1 - 0.4,
+                      child: Image.asset(
+                        "icons/danger-value.png",
+                        scale: 0.9,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            left: 0,
+            bottom: 0,
+            right: 0,
+            child: Container(
+              alignment: Alignment.center,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      bottom: 24,
+                      right: 10,
+                      left: 10,
+                    ),
+                    child: Divider(
+                      height: 10,
+                      color: Colors.black,
+                      thickness: 1,
+                    ),
+                  ),
+                  Padding(
+                      padding: const EdgeInsets.only(
+                        right: 10,
+                        bottom: 24,
+                        left: 10,
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(6),
+                        child: Container(
+                          color: Colors.white,
+                          child: Text("heeeelo"),
+                        ),
+                      )),
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
